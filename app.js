1d84f4a65bcd4733efd0aea245bef578941708db
titzko <matitzko@gmail.com>
2023-09-15 22:14:05 +0200

    initial

---
+++ app.js
@@ -0,0 +1,29 @@
+const markupInitialString = 
+`
+1. Wake up
+   - Check the time first
+   - If it's past 7 then wake up 
+2. Clean up 
+   - This includes brushing
+   - Taking a shower
+3. Eat
+   - Have cereals for breakfast
+`
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const showOutput = () => {
+        const markdownText = document.getElementById("markdown-content").innerText;
+        document.getElementById('html-output').innerHTML =
+            marked.parse(markdownText);
+    }
+
+    const initPage = () => {
+        document.querySelector('.CodeMirror').CodeMirror.setValue(markupInitialString)
+        showOutput();
+    }
+
+    document.getElementById('markdown-content').addEventListener('input', showOutput);
+    CodeMirror(document.getElementById('markdown-content'), {mode: 'markdown',}).setSize("100%", 610);
+    initPage();
+});
